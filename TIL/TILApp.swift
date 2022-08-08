3d487d539a3cfacd36e115ca804c87c0209d0e88
LimSeongHyeon <shot7351@gmail.com>
2022-08-09 00:31:56 +0900

    Initial Commit

---
+++ TIL/TILApp.swift
@@ -0,0 +1,17 @@
+//
+//  TILApp.swift
+//  TIL
+//
+//  Created by Lim on 2022/08/09.
+//
+
+import SwiftUI
+
+@main
+struct TILApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
